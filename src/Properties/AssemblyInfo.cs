8180843eceb9fab7499ab529b39923fadd67ba76
INDIVIS\artem.liman <artem.liman@inhive.group>
2017-11-24 21:30:04 +0100

    Added bump for version autoincrement

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompanyAttribute("inhive Group")]
+[assembly: AssemblyProduct("Trias.DataService")]
+[assembly: AssemblyTitle("Trias.DataService")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyDescription("Some description")]
+
